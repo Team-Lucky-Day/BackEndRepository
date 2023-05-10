eff180adee6ece44c5a62cd703ea15b710373b1b
JuJaeng2 <jjy5230@gmail.com>
2023-05-10 14:54:10 +0900

    Start Project

---
+++ src/test/java/LD_Caffe/ld_caffe/LdCaffeApplicationTests.java
@@ -0,0 +1,13 @@
+package LD_Caffe.ld_caffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdCaffeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
