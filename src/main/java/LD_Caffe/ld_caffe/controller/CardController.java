c651372db1f55b4990b66db697c0c32c50825d6a
romay <i_romay@naver.com>
2023-08-02 02:28:59 +0900

    CardController CORS 오류 수정

--- src/main/java/LD_Caffe/ld_caffe/controller/CardController.java
+++ src/main/java/LD_Caffe/ld_caffe/controller/CardController.java
@@ -7,14 +7,12 @@ import LD_Caffe.ld_caffe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/card")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class CardController {
 
     private final CardService cardService;
--- src/main/java/LD_Caffe/ld_caffe/service/CardService.java
+++ src/main/java/LD_Caffe/ld_caffe/service/CardService.java
@@ -10,6 +10,7 @@ public class CardService {
     private final CardRepository cardRepository;
 
     public boolean isCardVaild(String cardNum) {  // Card 가 DB에 존재하는지 조회
+        System.out.println("cardNum = " + cardNum);
         return !cardRepository.findByCardNumber(cardNum).isEmpty();
     }
 }
