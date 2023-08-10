c143442682aa620daf53bff49b928e81d60df1a4
jujaeyoung <jjy5230@gmail.com>
2023-08-10 10:52:09 +0900

    End Project

---
+++ src/main/java/LD_Caffe/ld_caffe/dto/FavoriteEnrollDto.java
@@ -0,0 +1,15 @@
+package LD_Caffe.ld_caffe.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FavoriteEnrollDto {
+
+    private int menuCode;
+
+}
